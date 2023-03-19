13de57711b3da5ea4b339381741bd564b9725082
lhyodo <leo.hyodo@outlook.com>
2023-03-18 19:06:50 -0700

    add 5 in a row algorithm

---
+++ IsStraight.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+int IsStraight(int *hand, int *community_cards, int HAND_SIZE, int COMMUNITY_SIZE)
+{
+    int ranks[13]{};
+
+    for (int i = 0; i < HAND_SIZE; ++i)
+    {
+        int c = hand[i];
+        c = c / 4;
+        ranks[c] += 1;
+    }
+
+    for (int i = 0; i < COMMUNITY_SIZE; ++i)
+    {
+        int c = community_cards[i];
+        c = c / 4;
+        ranks[c] += 1;
+    }
+    std::cout << "IsStraight: \n";
+    for (int i = 0; i < 13; ++i)
+    {
+        std::cout << ranks[i] << std::endl;
+    }
+
+    int indicator{};
+    for (int i = 4; i < 13; ++i)
+    {
+        if (ranks[i] > 0 && ranks[i - 1] > 0 && ranks[i - 2] > 0 && ranks[i - 3] > 0 && ranks[i - 4] > 0)
+        {
+            indicator = 1;
+        }
+    }
+
+    return indicator;
+}
