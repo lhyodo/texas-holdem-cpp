39e80495a205afc244fa9ca1e4625ead01a87d42
lhyodo <leo.hyodo@outlook.com>
2023-03-20 17:45:44 -0700

    add a 2 3 4 5 straight

--- IsStraight.cpp
+++ IsStraight.cpp
@@ -16,11 +16,11 @@ int IsStraight(int *hand, int *community_cards, int HAND_SIZE, int COMMUNITY_SIZ
         c = c / 4;
         ranks[c] += 1;
     }
-    std::cout << "IsStraight: \n";
-    for (int i = 0; i < 13; ++i)
-    {
-        std::cout << ranks[i] << std::endl;
-    }
+    // std::cout << "IsStraight: \n";
+    // for (int i = 0; i < 13; ++i)
+    // {
+    //     std::cout << ranks[i] << std::endl;
+    // }
 
     int indicator = -1;
     for (int i = 4; i < 13; ++i)
@@ -29,6 +29,10 @@ int IsStraight(int *hand, int *community_cards, int HAND_SIZE, int COMMUNITY_SIZ
         {
             indicator = i;
         }
+    }
+    if (ranks[12] > 0 && ranks[0] > 0 && ranks[1] > 0 && ranks[2] > 0 && ranks[3] > 0)
+    {
+        indicator = 3;
     }
 
     return indicator;
