5b16a3b94bff325796d4488b21dc6f4794c3f3c8
lhyodo <leo.hyodo@outlook.com>
2023-07-07 16:27:13 -0700

    comment

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -5,9 +5,10 @@
 #include <stdio.h>
 #include <iostream>
 #include <array>
+#include "Card.cpp"
 
 
-int Partition(int *arr, int low, int high)
+int Partition(Card *arr, int low, int high)
 {
     int pivot = low;
     int item_from_left = low;
@@ -25,7 +26,7 @@ int Partition(int *arr, int low, int high)
         }
         if (item_from_left < item_from_right)
         {
-            double temp = arr[item_from_left];
+            Card temp = arr[item_from_left];
             arr[item_from_left] = arr[item_from_right];
             arr[item_from_right] = temp;
             ++item_from_left;
@@ -33,23 +34,23 @@ int Partition(int *arr, int low, int high)
         }
     }
     // item_from_left has crossed item_from_right
-    double temp = arr[pivot];
+    Card temp = arr[pivot];
     arr[pivot] = arr[item_from_right];
     arr[item_from_right] = temp;
 
     return item_from_right; // returns the element that has found its definite position
 }
 
-void QuickSort(int *arr, int low, int high)
+void quickSort(Card *arr, int low, int high)
 {
-    if (low >= high) // base case
-        return;      // there is only one element
+    if (low >= high) // base case there is only one element
+        return;
     else
     {
         // recursive case
         int index = Partition(arr, low, high); // partition returns the element that has found its definite position
-        QuickSort(arr, low, index - 1);        // sort the group that is less than arr[index]
-        QuickSort(arr, index + 1, high);       // sort the group that is greater than arr[index]
+        quickSort(arr, low, index - 1);        // sort the group that is less than arr[index]
+        quickSort(arr, index + 1, high);       // sort the group that is greater than arr[index]
     }
     return;
 }
