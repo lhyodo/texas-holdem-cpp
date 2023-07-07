8a3cd9f9479945324dd713c3838d30b135a372db
lhyodo <leo.hyodo@outlook.com>
2023-07-06 22:36:30 -0700

    check or fold

--- main.cpp
+++ main.cpp
@@ -6,16 +6,32 @@
 #include "QuickSort.cpp"
 int main() {
   Dealer dealer{};
-  dealer.fillBoard();
-  dealer.displayBoard();
 
   std::vector<Player> vect_of_players{};
-  vect_of_players.push_back(Player());
+  vect_of_players.push_back(Player());  // player
+  vect_of_players.push_back(Player());  // computer
   dealer.fillHand(vect_of_players[0]);
+  dealer.fillHand(vect_of_players[1]);
   dealer.displayHand(vect_of_players[0]);
-  if (dealer.isPair(vect_of_players[0])) {
-    std::cout << "You have a pair!\n";
+
+  // check, call, raise, fold
+  std::cout << "please enter 'check', 'call', 'raise ##', or 'fold': ";
+  std::string input{};
+  std::cin >> input;
+
+  if (input == "check") {
+    dealer.fillBoard();
+    dealer.displayFlop();
+  } else if (input == "fold") {
+    dealer.resetDeck();
+    dealer.fillHand(vect_of_players[0]);
+    dealer.fillHand(vect_of_players[1]);
+    dealer.displayHand(vect_of_players[0]);
   }
+
+  // if (dealer.isPair(vect_of_players[0])) {
+  //   std::cout << "You have a pair!\n";
+  // }
 
   return 0;
 }
