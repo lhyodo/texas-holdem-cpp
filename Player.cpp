f027d82f12688497aa25588ce3fd35aaf363968b
lhyodo <leo.hyodo@outlook.com>
2023-07-07 15:33:48 -0700

    equalHand

--- Player.cpp
+++ Player.cpp
@@ -9,15 +9,29 @@ class Player {
   static const int TOP_SIZE = 5;
   static const int HAND_SIZE = 2;
   int chips = 500;
-  Card top_cards[TOP_SIZE]{};
+  int hand_points{};
+  Card primary_cards[TOP_SIZE]{};
+  Card secondary_cards[TOP_SIZE]{};
   Card hand[HAND_SIZE]{};
   Player() {
     for (int i = 0; i < TOP_SIZE; ++i) {
-      top_cards[i].setCard(-1);
+      primary_cards[i].setCard(-1);
+      secondary_cards[i].setCard(-1);
     }
     for (int i = 0; i < HAND_SIZE; ++i) {
       hand[i].setCard(-1);
     }
+  }
+  bool equalHand(Player &rhs) {
+    for (int i = 0; i < TOP_SIZE; ++i) {
+      if (primary_cards[i] != rhs.primary_cards[i]) {
+        return false;
+      }
+      if (secondary_cards[i] != rhs.secondary_cards[i]) {
+        return false;
+      }
+    }
+    return true;
   }
 };
 #endif
