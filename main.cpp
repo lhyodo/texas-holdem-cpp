cbd7e569fc38727f5414ac02f779cbd68fd82565
lhyodo <leo.hyodo@outlook.com>
2023-07-07 07:11:49 -0700

    while loop for rounds

--- main.cpp
+++ main.cpp
@@ -15,18 +15,61 @@ int main() {
   dealer.displayHand(vect_of_players[0]);
 
   // check, call, raise, fold
-  std::cout << "please enter 'check', 'call', 'raise ##', or 'fold': ";
   std::string input{};
-  std::cin >> input;
+  while (input != "quit") {
+    // flop
+    std::cout << "please enter 'check', 'call', 'raise ##', 'fold', or 'quit': ";
+    std::cin >> input;
+    if (input == "check") {
+      dealer.fillBoard();
+      dealer.displayFlop();
+    }
+    if (input == "quit") {
+      break;
+    }
+    if (input == "fold") {
+      dealer.fillHand(vect_of_players[0]);
+      dealer.fillHand(vect_of_players[1]);
+      dealer.displayHand(vect_of_players[0]);
+      continue;
+    }
 
-  if (input == "check") {
-    dealer.fillBoard();
-    dealer.displayFlop();
-  } else if (input == "fold") {
-    dealer.resetDeck();
-    dealer.fillHand(vect_of_players[0]);
-    dealer.fillHand(vect_of_players[1]);
-    dealer.displayHand(vect_of_players[0]);
+    // turn
+    std::cout << "please enter 'check', 'call', 'raise ##', 'fold', or 'quit': ";
+    std::cin >> input;
+    if (input == "check") {
+      dealer.displayTurn();
+    }
+    if (input == "quit") {
+      break;
+    }
+    if (input == "fold") {
+      dealer.fillHand(vect_of_players[0]);
+      dealer.fillHand(vect_of_players[1]);
+      dealer.displayHand(vect_of_players[0]);
+      continue;
+    }
+
+    // river
+    std::cout << "please enter 'check', 'call', 'raise ##', 'fold', or 'quit': ";
+    std::cin >> input;
+    if (input == "check") {
+      dealer.fillBoard();
+      dealer.displayRiver();
+      std::cout << "============================\n";
+      dealer.fillHand(vect_of_players[0]);
+      dealer.fillHand(vect_of_players[1]);
+      dealer.displayHand(vect_of_players[0]);
+    }
+    if (input == "quit") {
+      break;
+    }
+    if (input == "fold") {
+      dealer.fillHand(vect_of_players[0]);
+      dealer.fillHand(vect_of_players[1]);
+      dealer.displayHand(vect_of_players[0]);
+      continue;
+    }
   }
 
   // if (dealer.isPair(vect_of_players[0])) {
