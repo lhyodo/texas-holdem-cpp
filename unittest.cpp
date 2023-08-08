bdc26faec86a95fd31b991a76ae842e7a9120b04
lhyodo <leo.hyodo@outlook.com>
2023-08-08 12:09:30 -0700

    use hasNothing

--- unittest.cpp
+++ unittest.cpp
@@ -20,21 +20,22 @@ int main() {
     // 1 for diamond
     // 2 for hearts
     // 3 for spades
-    dealer.players[0].hand[0] = 2;
-    dealer.players[0].hand[1] = 45;
-    dealer.players[1].hand[0] = 18;
-    dealer.players[1].hand[1] = 31;
-    dealer.board[0] = 50;
-    dealer.board[1] = 46;
-    dealer.board[2] = 37;
-    dealer.board[3] = 6;
-    dealer.board[4] = 38;
+    dealer.players[0].hand[0] = 45;
+    dealer.players[0].hand[1] = 21;
+    dealer.players[1].hand[0] = 40;
+    dealer.players[1].hand[1] = 20;
+    dealer.board[0] = 2;
+    dealer.board[1] = 10;
+    dealer.board[2] = 39;
+    dealer.board[3] = 51;
+    dealer.board[4] = 12;
 
     dealer.displayHand(dealer.players[0]);
     dealer.displayBoard();
 
     dealer.displayHand(dealer.players[1]);
 
+    bool nothing_flag = dealer.hasNothing(dealer.players[0]);
     bool pair_flag = dealer.isPair(dealer.players[0]);
     bool dpair_flag = dealer.isDoublePair(dealer.players[0]);
     bool tkind_flag = dealer.isThreeKind(dealer.players[0]);
@@ -43,14 +44,15 @@ int main() {
     bool fhouse_flag = dealer.isFullHouse(dealer.players[0]);
     bool fkind_flag = dealer.isFourKind(dealer.players[0]);
     bool sflush_flag = dealer.isStraightFlush(dealer.players[0]);
-    std::cout << "pair_flag flag: " << pair_flag << std::endl;
-    std::cout << "dpair_flag flag: " << dpair_flag << std::endl;
-    std::cout << "tkind_flag flag: " << tkind_flag << std::endl;
-    std::cout << "fkind_flag flag: " << fkind_flag << std::endl;
-    std::cout << "flush_flag flag: " << flush_flag << std::endl;
-    std::cout << "fhouse_flag flag: " << fhouse_flag << std::endl;
-    std::cout << "straight flag: " << straight_flag << std::endl;
-    std::cout << "sflush_flag flag: " << sflush_flag << std::endl;
+    std::cout << "nothing_flag: " << nothing_flag << std::endl;
+    std::cout << "pair_flag: " << pair_flag << std::endl;
+    std::cout << "dpair_flag: " << dpair_flag << std::endl;
+    std::cout << "tkind_flag: " << tkind_flag << std::endl;
+    std::cout << "straight_flag: " << straight_flag << std::endl;
+    std::cout << "flush_flag: " << flush_flag << std::endl;
+    std::cout << "fhouse_flag: " << fhouse_flag << std::endl;
+    std::cout << "fkind_flag: " << fkind_flag << std::endl;
+    std::cout << "sflush_flag: " << sflush_flag << std::endl;
     std::cout << "==========\n";
     std::cout << "Primary Cards: \n";
     for (int i = 0; i < 5; ++i) {
